620624471da0cfeb7fc3d9231fa6b03a2095f4bf
Hymenium <bcshelbourne@gmail.com>
2026-07-21 13:54:52 +0100

    fix: Move geometry views into Geometry.cs

--- Box3d/Types/Geometry.cs
+++ Box3d/Types/Geometry.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Runtime.InteropServices;
 using Unity.Mathematics;
 
 namespace Box3d
 {
+    using Sys;
     /// <summary>Mirrors native b3Sphere (16 bytes): a sphere with a local-space offset.</summary>
     [StructLayout(LayoutKind.Sequential)]
     public struct Sphere
@@ -43,7 +45,7 @@ namespace Box3d
     [StructLayout(LayoutKind.Sequential)]
     public unsafe struct BoxHull
     {
-        internal Sys.b3HullData Base;
+        internal b3HullData Base;
         internal fixed byte Data[304];
 
         /// <summary>A box hull with the given half-extents, centered at the local origin.</summary>
@@ -69,6 +71,261 @@ namespace Box3d
         public static BoxHull CreateTransformed(float halfX, float halfY, float halfZ, B3Transform transform)
         {
             return Ffi.b3MakeTransformedBoxHull(halfX, halfY, halfZ, transform);
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public readonly struct HullHalfEdge
+    {
+        public readonly byte Next;
+        public readonly byte Twin;
+        public readonly byte Origin;
+        public readonly byte Face;
+    }
+
+    // ReadOnlySpan<float3> points = hull.Points;
+    // ReadOnlySpan<HullHalfEdge> edges = hull.HalfEdges;
+
+    // for (int i = 0; i < edges.Length; ++i)
+    // {
+    //     HullHalfEdge edge = edges[i];
+
+    //     if (i >= edge.Twin)
+    //         continue;
+
+    //     HullHalfEdge twin = edges[edge.Twin];
+
+    //     AddLine(
+    //         lines,
+    //         points[edge.Origin],
+    //         points[twin.Origin]);
+    // }
+    public readonly unsafe ref struct HullView
+    {
+        private readonly b3HullData* _data;
+
+        public HullView(b3HullData* data)
+        {
+            _data = data;
+        }
+
+        public int PointCount => _data->VertexCount;
+        public int HalfEdgeCount => _data->EdgeCount;
+        public int FaceCount => _data->FaceCount;
+
+        public ReadOnlySpan<float3> Points
+        {
+            get
+            {
+                byte* base_ptr = (byte*)_data;
+                float3* points =
+                    (float3*)(base_ptr + _data->PointOffset);
+
+                return new ReadOnlySpan<float3>(
+                    points,
+                    _data->VertexCount);
+            }
+        }
+
+        public ReadOnlySpan<HullHalfEdge> HalfEdges
+        {
+            get
+            {
+                byte* base_ptr = (byte*)_data;
+                HullHalfEdge* edges =
+                    (HullHalfEdge*)(base_ptr + _data->EdgeOffset);
+
+                return new ReadOnlySpan<HullHalfEdge>(
+                    edges,
+                    _data->EdgeCount);
+            }
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public readonly struct MeshTriangle
+    {
+        public readonly int Index1;
+        public readonly int Index2;
+        public readonly int Index3;
+    }
+
+    // ReadOnlySpan<float3> vertices = mesh.Vertices;
+    // ReadOnlySpan<MeshTriangle> triangles = mesh.Triangles;
+    // float3 scale = mesh.Scale;
+
+    // foreach (MeshTriangle triangle in triangles)
+    // {
+    //     float3 a = vertices[triangle.Index1] * scale;
+    //     float3 b = vertices[triangle.Index2] * scale;
+    //     float3 c = vertices[triangle.Index3] * scale;
+
+    //     AddLine(a, b);
+    //     AddLine(b, c);
+    //     AddLine(c, a);
+    // }
+    public readonly unsafe ref struct MeshView
+    {
+        private readonly b3MeshData* _data;
+        public float3 Scale { get; }
+
+        public MeshView(b3MeshData* data, float3 scale)
+        {
+            _data = data;
+            Scale = scale;
+        }
+
+        public MeshView(b3Mesh* mesh) : this(mesh->data, mesh->scale) { }
+
+        public ReadOnlySpan<float3> Vertices
+        {
+            get
+            {
+                b3MeshData* data = _data;
+                byte* base_ptr = (byte*)data;
+
+                return new ReadOnlySpan<float3>(
+                    (float3*)(base_ptr + data->vertexOffset),
+                    data->vertexCount);
+            }
+        }
+
+        public ReadOnlySpan<MeshTriangle> Triangles
+        {
+            get
+            {
+                b3MeshData* data = _data;
+                byte* base_ptr = (byte*)data;
+
+                return new ReadOnlySpan<MeshTriangle>(
+                    (MeshTriangle*)(base_ptr + data->triangleOffset),
+                    data->triangleCount);
+            }
+        }
+    }
+
+    public readonly unsafe ref struct HeightFieldView
+    {
+        private readonly b3HeightFieldData* _data;
+
+        public HeightFieldView(b3HeightFieldData* data)
+        {
+            _data = data;
+        }
+
+        public int ColumnCount => _data->columnCount;
+        public int RowCount => _data->rowCount;
+
+        public int CellCount =>
+            (ColumnCount - 1) * (RowCount - 1);
+
+        public int TriangleCount =>
+            2 * CellCount;
+
+        public float MinHeight => _data->minHeight;
+        public float MaxHeight => _data->maxHeight;
+        public float HeightScale => _data->heightScale;
+
+        public float3 Scale => _data->scale;
+
+        public bool Clockwise => _data->clockwise;
+
+        public ReadOnlySpan<ushort> CompressedHeights
+        {
+            get
+            {
+                byte* base_ptr = (byte*)_data;
+
+                return new ReadOnlySpan<ushort>(
+                    (ushort*)(base_ptr + _data->heightsOffset),
+                    ColumnCount * RowCount);
+            }
+        }
+
+        public ReadOnlySpan<byte> Materials
+        {
+            get
+            {
+                byte* base_ptr = (byte*)_data;
+
+                return new ReadOnlySpan<byte>(
+                    base_ptr + _data->materialOffset,
+                    CellCount);
+            }
+        }
+
+        public ReadOnlySpan<byte> TriangleFlags
+        {
+            get
+            {
+                byte* base_ptr = (byte*)_data;
+
+                return new ReadOnlySpan<byte>(
+                    base_ptr + _data->flagsOffset,
+                    TriangleCount);
+            }
+        }
+
+        public float GetHeight(int column, int row)
+        {
+            CheckGridPoint(column, row);
+
+            int index = row * ColumnCount + column;
+            ushort compressed = CompressedHeights[index];
+
+            return MinHeight + HeightScale * compressed;
+        }
+
+        public float3 GetPoint(int column, int row)
+        {
+            float height = GetHeight(column, row);
+
+            return new float3(
+                column * Scale.x,
+                height * Scale.y,
+                row * Scale.z);
+        }
+
+        public byte GetMaterial(int column, int row)
+        {
+            CheckCell(column, row);
+
+            int index = row * (ColumnCount - 1) + column;
+            return Materials[index];
+        }
+
+        private void CheckGridPoint(int column, int row)
+        {
+            if ((uint)column >= (uint)ColumnCount)
+                throw new ArgumentOutOfRangeException(nameof(column));
+
+            if ((uint)row >= (uint)RowCount)
+                throw new ArgumentOutOfRangeException(nameof(row));
+        }
+
+        public byte GetTriangleFlag(int column, int row, int triangle)
+        {
+            CheckCell(column, row);
+
+            if ((uint)triangle >= 2u)
+                throw new ArgumentOutOfRangeException(nameof(triangle));
+
+            int cell_index =
+                row * (ColumnCount - 1) + column;
+
+            int triangle_index =
+                2 * cell_index + triangle;
+
+            return TriangleFlags[triangle_index];
+        }
+
+        private void CheckCell(int column, int row)
+        {
+            if ((uint)column >= (uint)(ColumnCount - 1))
+                throw new ArgumentOutOfRangeException(nameof(column));
+
+            if ((uint)row >= (uint)(RowCount - 1))
+                throw new ArgumentOutOfRangeException(nameof(row));
         }
     }
 }
